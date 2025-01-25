dbe2987f423f19a2b62b958ca1a803847e82b7f2
Saken Meirzhan <meirzhansaken096@gmail.com>
2025-01-25 23:26:30 +0500

    Lists

---
+++ lab2/Lists/Copy_Lists.py
@@ -0,0 +1,15 @@
+#Make a copy of a list with the copy() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+#Make a copy of a list with the list() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+#Make a copy of a list with the : operator:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
